0f2c472276411daf0af6a5ee11ba22d1d2e8d4a1
AmaliyaMinnebaeva <90257176+AmaliyaMinnebaeva@users.noreply.github.com>
2022-05-29 09:08:16 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import telebot
 from telebot import types
 
 # Создаем бота
-bot = telebot.TeleBot('')
+bot = telebot.TeleBot('5348727287:AAGuYtgTjrLiSd6XIywhkRh_RqvWi3m6zlM')
 # Команда start
 @bot.message_handler(commands=["start"])
 def start(message):
